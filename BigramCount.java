f3b7c6d7fc585216920035d28587407097e2c704
Ange-Thierry Ishimwe <ai006@email.uark.edu>
2019-02-07 12:50:53 -0600

    added files

---
+++ BigramCount.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+public class BigramCount {
+
+	
+  public static class TokenizerMapper 
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    	
+	private boolean hasPassed; 
+	TokenizerMapper()
+	{
+		super();
+		hasPassed = false;
+	}
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private Text tracker = new Text("zz______________________bigrams Found_______________");
+    private  static Text track = new Text("zz______________________map Task Found_______________");
+    
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      
+	StringTokenizer itr = new StringTokenizer(value.toString());
+	
+	if(!hasPassed)
+	{
+		context.write(track,one);
+		hasPassed = true;
+	}
+	if(itr.countTokens() > 1)
+	{
+		String str1  = "";
+		String str2 = "";
+		String str3 = "";
+		while (itr.hasMoreTokens()) 
+		{
+			if(str1.isEmpty())
+				str1 = itr.nextToken();
+			str2 = itr.nextToken();
+			str3 = str1 + " " + str2;
+			word.set(str3);
+			context.write(word,one);
+			context.write(tracker,one);
+			str1 = str2;
+			str2 = "";
+			
+     		}
+
+	}
+      
+    }
+  }
+  public static class IntSumReducer 
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+    public void reduce(Text key, Iterable<IntWritable> values, 
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+	
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "word count");
+    job.setJarByClass(BigramCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	
+  }
+}
